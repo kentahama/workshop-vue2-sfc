bb1e507ac7882c5cb3c0813d9ff85bfd12806b84
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2022-10-14 15:24:13 +0900

    Add vue-loader to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require("node:path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
 };
